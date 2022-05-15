e87610be6e2d25a94cc82fb1d91bbba4708b6746
xiang ni <xiang_x_ni@manulife.com>
2022-05-15 18:24:26 +0800

    update

--- esm5/injector.abstract.d.ts
+++
@@ -1,7 +0,0 @@
-import { Provider, Type } from './type-api';
-export declare abstract class Injector {
-    abstract get<T = any>(target: any, ...params: any[]): T;
-    abstract set(token: any, provider: Provider): void;
-    abstract createClass<T = any>(clazz: Type<T>): T;
-    abstract clear(): void;
-}
--- esm5/local-storage.abstract.d.ts
+++
@@ -1,5 +0,0 @@
-import { Type } from "./type-api";
-export declare abstract class LocatorStorageImplements {
-    abstract getProvider<T = any>(target: any, ...params: any[]): T;
-    abstract getService<T>(target: Type<T>): T;
-}
