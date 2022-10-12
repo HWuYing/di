ad7d0c91cd1f054cb732e77d7b7459c4b0cb1729
xiang ni <xiang_x_ni@manulife.com>
2022-10-12 19:08:16 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-export * from './injectable';
-export * from './injector';
-export * from './injector.abstract';
-export * from './injector-token';
-export * from './type-api';
+export { Inject, Injectable, getProvider, registryProvider } from './injectable';
+export { StaticInjector } from './injector';
+export { Injector } from './injector.abstract';
+export { InjectorToken } from './injector-token';
+export { Provider, Type } from './type-api';
