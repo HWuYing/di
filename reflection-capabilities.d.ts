de16664023a722281cf9243ae8c88a1397574d12
xiang ni <xiang_x_ni@manulife.com>
2023-04-24 17:17:41 +0800

    update

--- reflection-capabilities.d.ts
+++ reflection-capabilities.d.ts
@@ -2,10 +2,10 @@ import 'reflect-metadata';
 import { Type } from './type-api';
 declare class ReflectionCapabilities {
     private _reflect;
-    getAnnotation(type: Type<any>, metadataName: string): any;
-    getParamAnnotations(type: Type<any>, methodName?: string): any[];
-    getMethodAnnotations(type: Type<any>, methodName: string): any;
-    getPropAnnotations(type: Type<any>, propName: string): any[];
+    getAnnotation<T = any>(type: Type<any>, metadataName: string): T | null;
+    getParamAnnotations<T = any>(type: Type<any>, methodName?: string): T[];
+    getMethodAnnotations<T = any>(type: Type<any>, methodName: string): T[];
+    getPropAnnotations<T = any>(type: Type<any>, propName: string): T[];
     parameters(type: Type<any>): any[];
     properties(type: Type<any>): any;
 }
